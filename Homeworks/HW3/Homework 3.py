8418b9b231732e406448fe20394f00d004b4936d
Jake Williamson <jw00006@mix.wvu.edu>
2024-12-04 22:19:53 -0500

    Initial commit for HW3, WIP need to fix user_input / input validation

--- Homeworks/HW3/Homework 3.py
+++ Homeworks/HW3/Homework 3.py
@@ -9,12 +9,17 @@ This will be our main screen
 
 def main():
     # Your code goes here.
-    main_link = "https://bugs.launchpad.net/ubuntu/+bugs?field.searchtext=&field.status%3Alist=EXPIRED&field.status%3Alist=CONFIRMED&field.status%3Alist=TRIAGED&field.status%3Alist=INPROGRESS&field.status%3Alist=FIXCOMMITTED&field.status%3Alist=FIXRELEASED&field.importance%3Alist=UNKNOWN&field.importance%3Alist=UNDECIDED&field.importance%3Alist=CRITICAL&field.importance%3Alist=HIGH&field.importance%3Alist=MEDIUM&field.importance%3Alist=LOW&field.importance%3Alist=WISHLIST&field.information_type%3Alist=PUBLIC&field.information_type%3Alist=PUBLICSECURITY&field.information_type%3Alist=PRIVATESECURITY&field.information_type%3Alist=USERDATA&assignee_option=any&field.assignee=&field.bug_reporter=&field.bug_commenter=&field.subscriber=&field.structural_subscriber=&field.component-empty-marker=1&field.tag=&field.tags_combinator=ANY&field.status_upstream-empty-marker=1&field.has_cve.used=&field.omit_dupes.used=&field.omit_dupes=on&field.affects_me.used=&field.has_no_package.used=&field.has_patch.used=&field.has_branches.used=&field.has_branches=on&field.has_no_branches.used=&field.has_no_branches=on&field.has_blueprints.used=&field.has_blueprints=on&field.has_no_blueprints.used=&field.has_no_blueprints=on&search=Search&orderby=-importance&memo=0&start=0"
+    main_link = "https://bugs.launchpad.net/ubuntu/+bugs?field.searchtext=&field.status%3Alist=EXPIRED&field.status%3Alist=CONFIRMED&field.status%3Alist=TRIAGED&field.status%3Alist=INPROGRESS&field.status%3Alist=FIXCOMMITTED&field.status%3Alist=FIXRELEASED&field.importance%3Alist=UNKNOWN&field.importance%3Alist=UNDECIDED&field.importance%3Alist=CRITICAL&field.importance%3Alist=HIGH&field.importance%3Alist=MEDIUM&field.importance%3Alist=LOW&field.importance%3Alist=WISHLIST&field.information_type%3Alist=PUBLIC&field.information_type%3Alist=PUBLICSECURITY&field.information_type%3Alist=PRIVATESECURITY&field.information_type%3Alist=USERDATA&assignee_option=any&field.assignee=&field.bug_reporter=&field.bug_commenter=&field.subscriber=&field.structural_subscriber=&field.component-empty-marker=1&field.tag=&field.tags_combinator=ANY&field.status_upstream-empty-marker=1&field.has_cve.used=&field.omit_dupes.used=&field.omit_dupes=on&field.affects_me.used=&field.has_no_package.used=&field.has_patch.used=&field.has_branches.used=&field.has_branches=on&field.has_no_branches.used=&field.has_no_branches=on&field.has_blueprints.used=&field.has_blueprints=on&field.has_no_blueprints.used=&field.has_no_blueprints=on&search=Search&orderby=-importance&"
     links = generate_links(main_link)
-    user_input = int(input('Which page would you like to view?'))
-    display_bugs_info(links[user_input])
-
-
+    while True:
+        try:
+            user_input = int(input(f'Which page would you like to view? (0 to {len(links) - 1}):'))
+            if 0 <= user_input <= len(links):
+                display_bugs_info(links[user_input])
+            else:
+                print("Invalid input. Please choose a valid page number.")
+        except ValueError:
+            print("Please enter a valid number.")
 
 def generate_links(start_link):
     """
@@ -23,13 +28,28 @@ def generate_links(start_link):
         :return: The list of all pages to all bugs. Each element in the list will refer to one page
     """
     list_of_links = []
+    pageCount = 0
+    bugsPerPage = 75
 
+    while True:
+        new_start = pageCount * bugsPerPage
+        new_page = f"{start_link}&start={new_start}"
 
+        web_tree = wb.get_web_tree(new_page)
+        bugRows = web_tree.xpath('//*[@class="buglisting-row"]')
+        if pageCount >= 5:  # Process only the first 10 pages for now
+            break
+        if not bugRows:
+            break
     # everytime you get a link to a new page, use list_of_links.append(new_page)
-    list_of_links(new_page)
+        list_of_links.append(new_page)
+        print(f"Fetching page {pageCount + 1}, URL: {new_page}")
+        pageCount += 1
+    # blankUrl = "https://bugs.launchpad.net/ubuntu/+bugs?field.searchtext=&field.status%3Alist=EXPIRED&field.status%3Alist=CONFIRMED&field.status%3Alist=TRIAGED&field.status%3Alist=INPROGRESS&field.status%3Alist=FIXCOMMITTED&field.status%3Alist=FIXRELEASED&field.importance%3Alist=UNKNOWN&field.importance%3Alist=UNDECIDED&field.importance%3Alist=CRITICAL&field.importance%3Alist=HIGH&field.importance%3Alist=MEDIUM&field.importance%3Alist=LOW&field.importance%3Alist=WISHLIST&field.information_type%3Alist=PUBLIC&field.information_type%3Alist=PUBLICSECURITY&field.information_type%3Alist=PRIVATESECURITY&field.information_type%3Alist=USERDATA&assignee_option=any&field.assignee=&field.bug_reporter=&field.bug_commenter=&field.subscriber=&field.structural_subscriber=&field.component-empty-marker=1&field.tag=&field.tags_combinator=ANY&field.status_upstream-empty-marker=1&field.has_cve.used=&field.omit_dupes.used=&field.omit_dupes=on&field.affects_me.used=&field.has_no_package.used=&field.has_patch.used=&field.has_branches.used=&field.has_branches=on&field.has_no_branches.used=&field.has_no_branches=on&field.has_blueprints.used=&field.has_blueprints=on&field.has_no_blueprints.used=&field.has_no_blueprints=on&search=Search&orderby=-importance&"
+
+    print(f"Generated {len(list_of_links)} links.")
 
     return list_of_links
-
 
 def display_bugs_info(bugs_page):
     """
@@ -37,7 +57,19 @@ def display_bugs_info(bugs_page):
         Display: Bug Number, Title, Importance, and Heat - in this order for each bug.(This is activities #11 and 12)
         :param bugs_page: The  link to the page
     """
+    web_tree = wb.get_web_tree(bugs_page)
+    bug_rows = web_tree.xpath('//*[@class="buglisting-row"]')
 
+    print(f"Displaying bugs for: {bugs_page}\n")
+    for bug in bug_rows:
+        bugImportance = bug.xpath('.//div["buglisting-col1"]//text()')
+        number = bugImportance[9].strip()
+        severity = bugImportance[1].strip()
+        status = bugImportance[3].strip()
+        heat = bugImportance[18].strip()
+        summary = bugImportance[11].strip()
+
+        print(f"{number}, {severity}, {status}, {heat}, {summary}")
 
 if __name__ == "__main__":
     main()
